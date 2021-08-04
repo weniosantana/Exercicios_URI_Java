7193e82fbd32b1b1f1027fd4a7b2c8fb8a38b071
weniosantana <wenio.souza123@gmail.com>
2021-08-04 19:00:55 -0300

    Adicionando exercicio novo

--- src/Uri_1037.java
+++ src/Uri_1037.java
@@ -1,37 +1,30 @@
 import java.io.IOException;
 import java.util.*;
+
 /**
- * IMPORTANT: 
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ * IMPORTANT: O nome da classe deve ser "Main" para que a sua solução execute
+ * Class name must be "Main" for your solution to execute El nombre de la clase
+ * debe ser "Main" para que su solución ejecutar
  */
 public class Uri_1037 {
- 
-    public static void main(String[] args) throws IOException {
- 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
-        Scanner sc = new Scanner(System.in);
-        double inter = sc.nextDouble();
-        if(inter >= 0 && inter <= 25){
-            System.out.println("Intervalo (0,25]");
-        }
-        else if(inter >= 25 && inter <= 50){
-            System.out.println("Intervalo (25,50]");
-        }
-        else if(inter >= 50 && inter <= 75){
-            System.out.println("Intervalo (50,75]");
-        }
-        else if(inter >= 75 && inter <= 100){
-            System.out.println("Intervalo (75,100]");
-        }
-        else {
-            System.out.println("Fora de intervalo");
-        }
-    }
- 
+
+	public static void main(String[] args) throws IOException {
+
+		/**
+		 * Escreva a sua solução aqui Code your solution here Escriba su solución aquí
+		 */
+		Scanner leitor = new Scanner(System.in);
+		double valor = leitor.nextDouble();
+		if (valor >= 0 && valor <= 25) {
+			System.out.println("Intervalo [0,25]");
+		} else if (valor > 25 && valor <= 50) {
+			System.out.println("Intervalo (25,50]");
+		} else if (valor > 50 && valor <= 75) {
+			System.out.println("Intervalo (50,75]");
+		} else if (valor > 75 && valor <= 100) {
+			System.out.println("Intervalo (75,100]");
+		} else {
+			System.out.println("Fora de intervalo");
+		}
+	}
 }
